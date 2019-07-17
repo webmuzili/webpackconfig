7c85d1239174462a93c162c548fb71bef02061c7
liyuan <1250953354@qq.com>
2019-07-17 11:04:17 +0800

    加了注释的是用于压缩代码，仅供参考

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const webpack=require('webpack')
 const path=require('path')
 const htmlWebpackPlugin=require('html-webpack-plugin')
+const UglifyJSPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const UglifyESPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports ={
     entry:path.join(__dirname,'./src/main.js'),
@@ -19,7 +23,49 @@ module.exports ={
         new htmlWebpackPlugin({
             template:path.join(__dirname,'./src/index.html'),
             filename:'index.html'
-        })
+        }),
+        new UglifyJSPlugin({
+            compress: {
+              // 在UglifyJs删除没有用到的代码时不输出警告
+              warnings: false,
+              // 删除所有的 `console` 语句，可以兼容ie浏览器
+              drop_console: true,
+              // 内嵌定义了但是只用到一次的变量
+              collapse_vars: true,
+              // 提取出出现多次但是没有定义成变量去引用的静态值
+              reduce_vars: true,
+            },
+            output: {
+              // 最紧凑的输出
+              beautify: false,
+              // 删除所有的注释
+              comments: false,
+            }
+          }),
+        new UglifyESPlugin({
+            // 多嵌套了一层
+            uglifyOptions: {
+              compress: {
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false,
+                // 删除所有的 `console` 语句，可以兼容ie浏览器
+                drop_console: true,
+                // 内嵌定义了但是只用到一次的变量
+                collapse_vars: true,
+                // 提取出出现多次但是没有定义成变量去引用的静态值
+                reduce_vars: true,
+              },
+              output: {
+                // 最紧凑的输出
+                beautify: false,
+                // 删除所有的注释
+                comments: false,
+              }
+            }
+          }),
+        new  ExtractTextPlugin({
+            filename: `[name]_[contenthash:8].css`,// 给输出的 CSS 文件名称加上 Hash 值
+          }),
     ],
     module:{
         rules:[
@@ -29,7 +75,15 @@ module.exports ={
             {test:/\.(jpg|png|gif|bmp|jpeg)$/,use: 'url-loader?limit=7631&name=[hash:8]-[name].[ext]'},
             { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' },
             { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.vue$/, use: 'vue-loader' }
+            { test: /\.vue$/, use: 'vue-loader' },
+            {
+                test: /\.css/,// 增加对 CSS 文件的支持
+                // 提取出 Chunk 中的 CSS 代码到单独的文件中
+                use: ExtractTextPlugin.extract({
+                  // 通过 minimize 选项压缩 CSS 代码
+                  use: ['css-loader?minimize']
+                }),
+              },
         ]
     },
     resolve: {
